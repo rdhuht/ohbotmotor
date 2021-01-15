8d8aa62228b5a70693c3e642b7ba20ebfbc28a0d
JeremyTsui <44908010+rdhuht@users.noreply.github.com>
2021-01-15 13:17:29 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,3 @@
-input.onButtonPressed(Button.A, function () {
-    眼睛俯仰角 += 1
-})
-input.onButtonPressed(Button.B, function () {
-    眼睛俯仰角 += -1
-})
 /**
  * s1, 脖子：0-180，右侧-左侧
  * 
@@ -18,9 +12,30 @@ input.onButtonPressed(Button.B, function () {
  * s6, 眼睛俯仰角：0-180，下-上
  * 
  * s7, 眼皮：0-180，闭眼-大眼，90中
+ * 
+ * p15, 眼睛颜色
  */
-let 眼睛俯仰角 = 0
-robotbit.Servo(robotbit.Servos.S1, 180)
+function speak (数字: number) {
+    for (let index = 0; index < 数字; index++) {
+        robotbit.Servo(robotbit.Servos.S3, 45)
+        robotbit.Servo(robotbit.Servos.S4, 45)
+        basic.pause(500)
+        robotbit.Servo(robotbit.Servos.S3, 0)
+        robotbit.Servo(robotbit.Servos.S4, 0)
+        basic.pause(200)
+    }
+}
+function blink (数字: number) {
+    for (let index = 0; index < 数字; index++) {
+        robotbit.Servo(robotbit.Servos.S7, 0)
+        basic.pause(200)
+        robotbit.Servo(robotbit.Servos.S7, 180)
+        basic.pause(500)
+    }
+}
+music.setVolume(255)
+soundExpression.yawn.playUntilDone()
 basic.forever(function () {
-    serial.writeValue("eyelids", 眼睛俯仰角)
+    basic.pause(100)
+    blink(3)
 })
